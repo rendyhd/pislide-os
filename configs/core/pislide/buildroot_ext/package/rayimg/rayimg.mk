6226df0db3dd64fd17a041d32a14af4084b8f730
Jarvy Jarvison <88567611+JarvyJ@users.noreply.github.com>
2025-09-02 22:54:10 -0500

    Switch to new teal-based rayimg (#30)
    add package for new lua/teal-based rayimg
    add package for libraylib
    add package for lua-vips
    update libvips
    remove pislideapi

--- configs/core/pislide/buildroot_ext/package/rayimg/rayimg.mk
+++
@@ -1,9 +0,0 @@
-RAYIMG_VERSION = 0.0.4
-RAYIMG_SITE = https://github.com/JarvyJ/rayimg
-RAYIMG_SITE_METHOD = git
-RAYIMG_GOMOD = github.com/JarvyJ/rayimg
-RAYIMG_BUILD_TARGETS += cmd/rayimg
-RAYIMG_INSTALL_BINS += rayimg
-RAYIMG_DEPENDENCIES += libgbm libdrm
-RAYIMG_TAGS += drm,noaudio,rpi
-$(eval $(golang-package))
