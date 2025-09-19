512621993e868910f07600259bc5ea0c991d3dd0
Jarvy Jarvison <88567611+JarvyJ@users.noreply.github.com>
2025-09-18 19:32:22 -0500

    Update to rayimg release (#32)
    - [x] Updated to latest rayimg
    - [x] removed old libdrm patch (I _think_ it was mostly for compiling
    raylib linking libdrm with golang)

--- configs/core/pislide/buildroot_ext/package/lua-rayimg/lua-rayimg.mk
+++ configs/core/pislide/buildroot_ext/package/lua-rayimg/lua-rayimg.mk
@@ -6,9 +6,14 @@
 
 LUA_RAYIMG_VERSION = 1.0.0-1
 LUA_RAYIMG_NAME_UPSTREAM = rayimg
-LUA_RAYIMG_SITE = https://github.com/JarvyJ/rayimg/releases/download/1.0.0-pre
+LUA_RAYIMG_SITE = https://github.com/JarvyJ/rayimg/releases/download/1.0.0
 LUA_RAYIMG_LICENSE = AGPL
 LUA_RAYIMG_SUBDIR = rayimg
 LUA_RAYIMG_LICENSE_FILES = $(LUA_RAYIMG_SUBDIR)/LICENSE
+LUA_RAYIMG_INSTALL_TARGET_CMDS = y
+
+define LUA_RAYIMG_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/$(LUA_RAYIMG_SUBDIR)/static/rayimg $(TARGET_DIR)/usr/bin/rayimg
+endef
 
 $(eval $(luarocks-package))
